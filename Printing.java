3928c2b2d4e7801c2f8c25e1bd42227b99fed3f4
LHNT2k7 <ltri9573@gmail.com>
2024-09-03 10:11:01 -0700

    Lan Dau tien

---
+++ Printing.java
@@ -0,0 +1,22 @@
+public class Printing
+{
+    public static void main(String[] args)
+    {
+        // int, double, boolean, String
+        int myAge = 999; //whole numbers
+        double price = 4.99; 
+        
+        
+        boolean isGood; // variable declaration
+        isGood = false; // variable initialization
+
+        String name = "Tri Le";
+        
+        
+        
+        System.out.print("Hello, my name is");
+
+    }
+}
+
+
